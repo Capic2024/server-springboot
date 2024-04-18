7a20ec11ce2a356ddb938769202d554937c252f8
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-04-18 18:24:24 +0900

    feat : cors 수정

--- src/main/java/com/capic/server/global/config/CorsConfig.java
+++ src/main/java/com/capic/server/global/config/CorsConfig.java
@@ -3,7 +3,13 @@ package com.capic.server.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,6 +19,21 @@ import java.util.List;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class CorsConfig {
+
+    /**
+     * FilterChain 설정
+     * @param http - 시큐리티 설정을 담당하는 객체
+     * @return - FilterChain 진행 값 반환
+     * @throws Exception - 시큐리티 설정 관련 모든 예외 (JWT 관련 포함)
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // CORS 허용, CSRF 비활성화
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
     /**
      * CORS 허용하도록 커스터마이징 진행
      * @return - 변경된 CORS 정책 정보 반환
